3de7fb7a62e66d7ea415935553501cf8558aa9a9
Steven Olmsted <steven.olmsted@ibigroup.com>
2019-02-17 20:01:07 -0500

    Fix eslint comments

--- js/for-in.js
+++ js/for-in.js
@@ -1,6 +1,7 @@
-/* eslint-disable guard-for-in */
 export default (object, iterationFunction) => {
+    /* eslint-disable guard-for-in */
     for (const propertyName in object) {
+    /* eslint-enable guard-for-in */
         iterationFunction(object[propertyName], propertyName, object);
     }
 };
