3414650393218c8dc2ecfb580f0a28e7f01085c9
Steven Olmsted <steven.olmsted@ibigroup.com>
2020-07-27 14:37:10 -0400

    Lint cleanup

--- js/for-in.js
+++ js/for-in.js
@@ -1,5 +1,5 @@
 export default (object, iterationFunction) => {
-    for (const propertyName in object) { // eslint-disable-line guard-for-in
+    for (const propertyName in object) { // eslint-disable-line guard-for-in -- The for-in loop here is intentionally used to iterate the prototype chain, so it should not be guarded against doing so.
         iterationFunction(object[propertyName], propertyName, object);
     }
 };
